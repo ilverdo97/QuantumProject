a92c56a3117bafcbaf6c5b5bc9387fb1a3669e91
Luca Verdolini <34744067+ilverdo97@users.noreply.github.com>
2022-05-25 09:44:31 +0200

    Operations

---
+++ division.py
@@ -0,0 +1,31 @@
+import math
+from qiskit import *
+from operations import addition, subtraction
+
+pie = math.pi
+
+
+
+def div(dividend, divisor, accumulator,c_dividend, circ, cl_index):
+    d = QuantumRegister(1)
+    circ.add_register(d)
+    circ.x(d[0])
+
+    c_dividend_str = '0'
+
+    while c_dividend_str[0] == '0':
+        subtraction.sub(dividend, divisor, circ)
+        addition.sum(accumulator, d, circ)
+
+        for i in range(len(dividend)):
+            circ.measure(dividend[i], c_dividend[i])
+
+        result = execute(circ, backend=Aer.get_backend('qasm_simulator'),
+                         shots=10).result()
+ 
+        counts = result.get_counts("qc")
+        #print(counts)
+        c_dividend_str = list(counts.keys())[0] #.split()[0]
+     
+
+    subtraction.sub(accumulator, d, circ)
---
+++ multiplication.py
@@ -0,0 +1,25 @@
+import math
+from qiskit import *
+from utils import bcolors, executeQFT, evolveQFTStateSum, inverseQFT
+
+pie = math.pi
+
+def multiply(a, secondDec, result, qc):
+    
+    n = len(a) -1
+    # Compute the Fourier transform of register 'result'
+    for i in range(n+1):
+        executeQFT(qc, result, n-i, pie)
+    
+    # Add the two numbers by evolving the Fourier transform F(ψ(reg_a))>
+    # to |F(ψ((second * reg_a))>, where we loop on the sum as many times as 'second' says, 
+    # doing incremental sums
+    for j in range(secondDec):
+        for i in range(n+1):
+            evolveQFTStateSum(qc, result, a, n-i, pie)
+
+    # Compute the inverse Fourier transform of register a
+    for i in range(n+1):
+        inverseQFT(qc, result, i, pie)
+
+   
